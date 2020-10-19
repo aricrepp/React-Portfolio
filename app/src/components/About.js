c105c65289fb930b0bfc19d4d4de37790df2d908
Aric Repp <aricmrepp@gmail.com>
2020-10-18 23:50:00 -0400

    pushing new changes

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
 import './css/About.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -19,7 +20,37 @@ const useStyles = makeStyles((theme) => ({
     padding: '0.5rem',
     width: '60%',
   },
+  skills: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+    width: '60%',
+    padding: '15px',
+  },
 }));
+
+const skills = [
+  'HTML5',
+  'CSS3',
+  'Javascript',
+  'Agile',
+  'LESS',
+  'GIT',
+  'Github',
+  'MaterialUI',
+  'React',
+  'Redux',
+  'Jest',
+  'Cypress',
+  'SQLLite',
+  'PostGreSQL',
+  'Node',
+  'ExpressJS',
+  'Python',
+];
 
 const About = () => {
   const classes = useStyles();
@@ -37,6 +68,11 @@ const About = () => {
         <CardContent className={classes.content}>
           <Typography className={classes.projects}>Hello!</Typography>
         </CardContent>
+        <div className={classes.skills}>
+          {skills.map((e) => {
+            return <Chip label={e} color={'primary'} />;
+          })}
+        </div>
       </Card>
     </div>
   );
